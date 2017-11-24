b80788750a51ecb2d898e26dc31aa85dad311653
EwenCH <ewench@users.noreply.github.com>
2017-11-24 15:02:51 +0000

    Implemented slides that appear on first start.

--- unwind-app/app/src/main/java/unwind/unwind/solutions/unwind/MainActivity.java
+++ unwind-app/app/src/main/java/unwind/unwind/solutions/unwind/MainActivity.java
@@ -1,13 +1,44 @@
 package unwind.unwind.solutions.unwind;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    public static final String PREF_KEY_FIRST_START =
+            "com.unwind.app.PREF_KEY_FIRST_START";
+    public static final int REQUEST_CODE_INTRO = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        boolean firstStart = PreferenceManager.getDefaultSharedPreferences(this)
+                .getBoolean(PREF_KEY_FIRST_START, true);
+
+        if (firstStart) {
+            Intent intent = new Intent(this, MainIntroActivity.class);
+            startActivityForResult(intent, REQUEST_CODE_INTRO);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_INTRO) {
+            if (resultCode == RESULT_OK) {
+                PreferenceManager.getDefaultSharedPreferences(this).edit()
+                        .putBoolean(PREF_KEY_FIRST_START, false)
+                        .apply();
+            } else {
+                PreferenceManager.getDefaultSharedPreferences(this).edit()
+                        .putBoolean(PREF_KEY_FIRST_START, true)
+                        .apply();
+            }
+        }
     }
 }
--- unwind-app/app/src/main/java/unwind/unwind/solutions/unwind/MainIntroActivity.java
+++ unwind-app/app/src/main/java/unwind/unwind/solutions/unwind/MainIntroActivity.java
@@ -1,9 +1,11 @@
 package unwind.unwind.solutions.unwind;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
 
 public class MainIntroActivity extends IntroActivity {
@@ -14,7 +16,24 @@ public class MainIntroActivity extends IntroActivity {
 
         //Welcome slide.
         addSlide(new SimpleSlide.Builder()
-        .build());
+                .title(R.string.app_name)
+                .description(R.string.introduction_text1)
+                .image(R.drawable.mi_ic_next) //TODO - Placeholder image.
+                .background(R.color.colorPrimary)
+                .backgroundDark(R.color.colorPrimaryDark)
+                .scrollable(false)
+                .build());
+
+        //Description slide 1.
+        addSlide(new SimpleSlide.Builder()
+                .title(R.string.app_name)
+                .description(R.string.introduction_text2)
+                .image(R.drawable.mi_ic_previous) //TODO - Placeholder image.
+                .background(R.color.colorPrimary)
+                .backgroundDark(R.color.colorPrimaryDark)
+                .scrollable(false)
+                .build());
+
     }
 
 }
