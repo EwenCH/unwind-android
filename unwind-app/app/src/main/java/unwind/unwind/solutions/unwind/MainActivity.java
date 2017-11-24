180d58b1b9a7f35840b5edce08c7612a0cc5d7bd
EwenCH <ewench@users.noreply.github.com>
2017-11-24 16:10:39 +0000

    Added IBM Watson SDK and updated colours

--- unwind-app/app/src/main/java/unwind/unwind/solutions/unwind/MainActivity.java
+++ unwind-app/app/src/main/java/unwind/unwind/solutions/unwind/MainActivity.java
@@ -7,12 +7,10 @@ import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    public static final String PREF_KEY_FIRST_START =
-            "com.unwind.app.PREF_KEY_FIRST_START";
+    public static final String PREF_KEY_FIRST_START = "com.unwind.app.PREF_KEY_FIRST_START";
     public static final int REQUEST_CODE_INTRO = 1;
 
     @Override
@@ -20,9 +18,11 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Find out if this is the first time the app is being opened.
         boolean firstStart = PreferenceManager.getDefaultSharedPreferences(this)
                 .getBoolean(PREF_KEY_FIRST_START, true);
 
+        //If it is, call the introduction activity.
         if (firstStart) {
             Intent intent = new Intent(this, MainIntroActivity.class);
             startActivityForResult(intent, REQUEST_CODE_INTRO);
@@ -33,6 +33,9 @@ public class MainActivity extends Activity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_CODE_INTRO) {
+
+            //If the introduction activity has finished, then save the first start boolean
+            //as false.
             if (resultCode == RESULT_OK) {
                 PreferenceManager.getDefaultSharedPreferences(this).edit()
                         .putBoolean(PREF_KEY_FIRST_START, false)
@@ -54,6 +57,8 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        //TODO - Temporary Dev option, remove for production.
+        //Set the first start variable to be true if the menu item is selected.
         if (item.getItemId() == R.id.reset_first_start_menu) {
             PreferenceManager.getDefaultSharedPreferences(this).edit()
                     .putBoolean(PREF_KEY_FIRST_START, true)
