1b5f4fd83b904bb3b254b30c94dff76bab350cfd
EwenCH <ewench@users.noreply.github.com>
2017-11-24 13:02:33 +0000

    Added Android project to repo.

---
+++ unwind-app/app/src/main/java/unwind/unwind/solutions/unwind/MainActivity.java
@@ -0,0 +1,13 @@
+package unwind.unwind.solutions.unwind;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
